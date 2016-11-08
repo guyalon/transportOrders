46f3e9808ac6c19e9aeb6c59647d27b638a0bfb5
Guy Alon <guyalon@vocativ.com>
2016-11-08 18:27:44 +0200

    all inputs are ready 1.5hrs

--- scripts/main.js
+++ scripts/main.js
@@ -37,9 +37,30 @@ var Components = function () {
         return $(select);
     }
 
+    var buttonTemplate = [
+        '<div class="input-wrapper">',
+            '<button class="send-button">שלח</button>',
+        '</div>'
+    ].join('');
+
+
+    var generateTextarea = function (inputName,label,placeholder,className) {
+
+        var input = [
+            '<div class="input-wrapper">',
+            '<label for="'+ label +'">'+label+'</label>',
+            '<textarea placeholder="'+placeholder+'" name="'+inputName+'" class="'+className+'" />',
+            '</div>'
+        ].join('');
+        return $(input);
+    };
+
+
     return {
         generateInput:generateInput,
-        generateSelect:generateSelect
+        generateSelect:generateSelect,
+        button:buttonTemplate,
+        generateTextarea:generateTextarea
     }
 };
 
@@ -47,21 +68,42 @@ var components = new Components();
 
 $(document).ready(function () {
 
-    $('.orders-form-wrapper').append(components.generateInput('date','date_of_arrival','תאריך נחיתה','','date-of-arrival'));
-    $('.orders-form-wrapper').append(components.generateInput('time','time_of_arrival','שעת נחיתה','נא הקש את שעת הנחיתה','time-of-arrival'));
-
     var airportSelectValuesArray = [{val:'fco',label:'Leonardo da Vinci - Fiumicino'},{val:'cia',label:'Ciampiano Airport'},{val:'ctp',label:'Civitavecchia Port'}]
-    $('.orders-form-wrapper').append(components.generateSelect('נחיתה בשדה תעופה / נמל','port-select','port-select',airportSelectValuesArray));
-
-    $('.orders-form-wrapper').append(components.generateInput('text','airline_name','שם חברת התעופה/שייט','נא הקש את שם חברת התעופה/שייט','airline-name'));
-    $('.orders-form-wrapper').append(components.generateInput('text','flight_number','מספר טיסה','נא הקש את מספר הטיסה','flight-number'));
-    $('.orders-form-wrapper').append(components.generateInput('text','origin','הגעה מ','נא הקש את מיקום היציאה','origin'));
-    $('.orders-form-wrapper').append(components.generateInput('text','destination','יעד ברומא','כתובת מלון או דירה ברומא','destination'));
-
-
     var nopSelectValuesArray = [{val:'1',label:'1'},{val:'2',label:'2'},{val:'3',label:'3'},{val:'4',label:'4'},{val:'5',label:'5'},{val:'6',label:'6'},{val:'7',label:'7'},{val:'8',label:'+8'}]
-    $('.orders-form-wrapper').append(components.generateSelect('מספר אנשים','number_of_people','number-of-people',nopSelectValuesArray ));
 
+
+    // Arrivals
+    $('.orders-form-wrapper .arrivals').append(components.generateInput('date','date_of_arrival','תאריך נחיתה','','date-of-arrival'));
+    $('.orders-form-wrapper .arrivals').append(components.generateInput('time','time_of_arrival','שעת נחיתה','נא הקש את שעת הנחיתה','time-of-arrival'));
+    $('.orders-form-wrapper .arrivals').append(components.generateSelect('נחיתה בשדה תעופה / נמל','arrival-port','arrival-port',airportSelectValuesArray));
+    $('.orders-form-wrapper .arrivals').append(components.generateInput('text','arrival_airline_name','שם חברת התעופה/שייט','נא הקש את שם חברת התעופה/שייט','airline-name'));
+    $('.orders-form-wrapper .arrivals').append(components.generateInput('text','arrival_flight_number','מספר טיסה','נא הקש את מספר הטיסה','flight-number'));
+    $('.orders-form-wrapper .arrivals').append(components.generateInput('text','origin','הגעה מ','נא הקש את מיקום היציאה','origin'));
+    $('.orders-form-wrapper .arrivals').append(components.generateInput('text','destination','יעד ברומא','כתובת מלון או דירה ברומא','destination'));
+    $('.orders-form-wrapper .arrivals').append(components.generateSelect('מספר אנשים','number_of_people','number-of-people',nopSelectValuesArray ));
+
+
+    // Departure
+    $('.orders-form-wrapper .departures').append(components.generateInput('date','date_of_departure','מועד המראה','','date-of-departure'));
+    $('.orders-form-wrapper .departures').append(components.generateInput('time','time_of_flight','שעת המראה','נא הקש את שעת ההמראה','time-of-departure'));
+    $('.orders-form-wrapper .departures').append(components.generateSelect('נחיתה בשדה תעופה / נמל','departure_port','departure-port',airportSelectValuesArray));
+    $('.orders-form-wrapper .departures').append(components.generateInput('text','departure_airline_name','שם חברת התעופה/שייט','נא הקש את שם חברת התעופה/שייט','airline-name'));
+    $('.orders-form-wrapper .departures').append(components.generateInput('text','departure_flight_number','מספר טיסה','נא הקש את מספר הטיסה','flight-number'));
+    $('.orders-form-wrapper .departures').append(components.generateInput('text','pickup_location','הגעה מ','נא הקש את מיקום היציאה','origin'));
+    $('.orders-form-wrapper .departures').append(components.generateInput('text','final_destination','יעד ברומא','כתובת מלון או דירה ברומא','destination'));
+    $('.orders-form-wrapper .departures').append(components.generateInput('time','pickup_time','שעת איסוף','','time-of-pickup'));
+    $('.orders-form-wrapper .departures').append(components.generateSelect('מספר אנשים','departure_number_of_people','number-of-people',nopSelectValuesArray ));
+
+
+
+    $('.orders-form-wrapper .personal-details').append(components.generateInput('text','full_name','שם מלא','נא הקש את שמך המלא באנגלית','full-name'));
+    $('.orders-form-wrapper .personal-details').append(components.generateInput('phone','rome_phone_number','מספר טלפון ברומא','נא הקש את מספר הטלפון ברומא','rome-phone-number'));
+    $('.orders-form-wrapper .personal-details').append(components.generateInput('phone','israel_phone_number','מספר טלפון בישראל','נא הקש את מספר הטלפון בישראל','israel-phone-number'));
+    $('.orders-form-wrapper .personal-details').append(components.generateInput('email','email','כתובת אימייל','נא הקש את כתובת האימייל','email'));
+    $('.orders-form-wrapper .personal-details').append(components.generateTextarea('notes','הערות','הערות','notes'));
+
+
+    $('.orders-form-wrapper').append(components.button);
 
 
 
